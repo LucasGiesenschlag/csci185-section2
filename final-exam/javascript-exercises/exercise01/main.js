17976f254dffd0897c12ef92ea2d48b5e73e0888
LucasGiesenschlag <lgiesens@unca.edu>
2023-04-28 13:30:42 -0400

    final exam

---
+++ final-exam/javascript-exercises/exercise01/main.js
@@ -0,0 +1,23 @@
+function color1() {
+    document.querySelector('#square1').style.backgroundColor = "pink"
+}
+
+function color2() {
+    document.querySelector('#square2').style.backgroundColor = "brown"
+}
+
+function color3() {
+    document.querySelector('#square3').style.backgroundColor = "orange"
+}
+
+function color4() {
+    document.querySelector('#square4').style.backgroundColor = "yellow"
+}
+
+function color5() {
+    document.querySelector('#square5').style.backgroundColor = "red"
+}
+
+function color6() {
+    document.querySelector('#square6').style.backgroundColor = "purple"
+}
